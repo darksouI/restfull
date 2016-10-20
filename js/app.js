bf7b4b06c5e1160c7f32f2f29d9e9a6152e470aa
dark soul <domenico.rocciolo@gmail.com>
2016-10-20 12:01:02 +0200

    funzione ricerca

--- js/app.js
+++ js/app.js
@@ -9,6 +9,20 @@ $(document).ready(function() {
       url: 'http//:www.omdbapi.com/?s='+ titolo,
       method: 'GET'
     }).then(function(data) {
+        var tr = "";
+        for (var i = 0; i < data.Search.length; i++) {
+            var obj = data.Search[i];
+            tr += "<tr"+obj.Title+"'>";
+            tr +="<td>" + obj.Year + "</td>";
+            tr +="<td>" + obj.imdbID + "</td>";
+            tr +="<td>" + obj.Type + "</td>";
+            tr +="<td>" + obj.Poster + "</td>";
+            tr +="</tr>";
+
+            //console.log(tr);
+            $.LoadingOverlay("hide");
+        }
+        $("#").html(tr);
       console.log(data);
     });
 
