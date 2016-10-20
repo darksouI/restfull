7e240b6e2455eef4b5fd1acd64f1129fc5a8991d
dark soul <domenico.rocciolo@gmail.com>
2016-10-20 14:53:12 +0200

    aggiunta loadingOverlay

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@ $(document).ready(function() {
     $("#search").submit(function(event){
       event.preventDefault();
 
+      $.LoadingOverlay("show", {
+        fontawesome: "fa fa-spinner fa-spin"
+      });
+
 
     var titolo = $("input[name='titolo']").val();
     $.ajax({
@@ -14,21 +18,23 @@ $(document).ready(function() {
             var obj = data.Search[i];
             tr += "<tr"+obj.Title+">";
             if (obj.Poster == 'N/A') {
-              tr +='<td><img class="img" src="http://placehold.it/350x150"/></td>'
+              tr +='<td class="col-sm-2"><img class="img" src="http://placehold.it/350x150"/></td>'
             }else{
-            tr +="<td><img class='img' src='" + obj.Poster + "'/></td>";
+            tr +="<td class='col-sm-2'><img class='img' src='" + obj.Poster + "'/></td>";
           }
-          tr +="<td>" + obj.Year + "</td>";
-          tr +="<td>" + obj.imdbID + "</td>";
-          tr +="<td>" + obj.Type + "</td>";
+          tr +="<td class='col-sm-2'>" + obj.Year + "</td>";
+          tr +="<td class='col-sm-2'>" + obj.imdbID + "</td>";
+          tr +="<td class='col-sm-2'>" + obj.Type + "</td>";
             tr +="</tr>";
 
             //console.log(tr);
-            //$.LoadingOverlay("hide");
+            $.LoadingOverlay("hide");
         }
         $("#tb_imdb").html(tr);
       //console.log(data);
   });
     });
+
+
 
 });
